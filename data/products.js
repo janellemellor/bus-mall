779cdc82ca9132d8f694188059e13b4cae8ceff5
janelle mellor <janellemellor@gmail.com>
2020-01-28 09:54:26 -0800

    add product data

---
+++ data/products.js
@@ -0,0 +1,123 @@
+
+export const productData = [
+    { 
+        id: '1',
+        image: 'bag.jpg',
+        name: 'R2-D2 bag',
+    },
+
+    {
+        id: '2',
+        image: 'banana.jpg',
+        name: 'banana slicer',
+    },
+
+    {
+        id: '3',
+        image: 'bathroom.jpg',
+        name: 'ipad toilet paper holder',
+    },
+
+    {
+        id: '4',
+        image: 'boots.jpg',
+        name: 'open toed rain boots',
+    },
+
+    {
+        id: '5',
+        image: 'breakfast.jpg',
+        name: 'breakfast station',
+    },
+
+    {
+        id: '6',
+        image: 'bubblegum.jpg',
+        name: 'meatball bubble gum',
+    },
+
+    {
+        id: '7',
+        image: 'chiar.jpg',
+        name: 'red chair',
+    },
+
+    {
+        id: '8',
+        image: 'cthulhu.jpg',
+        name: 'cthulhu action figure',
+    },
+
+    {
+        id: '9',
+        image: 'dog-duck.jpg',
+        name: 'dog duck bill',
+    },
+
+    {
+        id: '10',
+        image: 'dragon.jpg',
+        name: 'dragon meat',
+    },
+
+    {
+        id: '11',
+        image: 'pen.jpg',
+        name: 'utensil pen',
+    },
+
+    {
+        id: '12',
+        image: 'pet-sweep.jpg',
+        name: 'pet dust boots',
+    },
+
+    {
+        id: '13',
+        image: 'scissors.jpg',
+        name: 'pizza scissors',
+    },
+
+    {
+        id: '14',
+        image: 'shark.jpg',
+        name: 'shark sleeping bag',
+    },
+
+    {
+        id: '15',
+        image: 'sweep.jpg',
+        name: 'baby sweeper',
+    },
+
+    {
+        id: '16',
+        image: 'tauntaun.jpg',
+        name: 'tauntaun sleeping bag',
+    },
+
+    {
+        id: '17',
+        image: 'unicorn.jpg',
+        name: 'unicorn meat',
+    },
+
+    {
+        id: '18',
+        image: 'usb.jpg',
+        name: 'octopus usb',
+    },
+
+    {
+        id: '19',
+        image: 'water-can.jpg',
+        name: 'self-watering water can',
+    },
+
+    {
+        id: '20',
+        image: 'wine-glass.jpg',
+        name: 'wine glass',
+    },
+
+];
